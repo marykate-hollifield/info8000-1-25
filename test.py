681b77fb1db32c3f82c0eb240d23de6434f69dd0
Mary Kate Hollifield <hollifieldmarykate@gmail.com>
2022-01-27 12:09:49 -0500

    added a first example of multiple ways to iterate and sum

---
+++ test.py
@@ -0,0 +1,17 @@
+#print("hello world")
+#print("Loading the file")
+f = open("test.txt")
+lines = f.readlines()
+#print(lines)
+
+the_sum = 0
+
+#for line in lines:
+ #   print(line)
+
+for line in lines:
+    the_sum = the_sum + int(line)
+    print(the_sum)
+
+print(sum([int(x) for x in lines]))
+  
