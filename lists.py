bbbf21136b13c1f299b0a3ba0e06d0a30756aca4
Mary Kate Hollifield <hollifieldmarykate@gmail.com>
2022-02-01 11:40:59 -0500

    started list access

---
+++ lists.py
@@ -0,0 +1,4 @@
+#lists are the most common type of data structure in python
+#Dictionary is the second most
+a_list = [34,13,95]
+print(a_list[0])
--- test.py
+++ test.py
@@ -1,17 +1,22 @@
 #print("hello world")
 #print("Loading the file")
-f = open("test.txt")
+f = open("test.txt",encoding='utf-8')
 lines = f.readlines()
-#print(lines)
+print(lines)
+lines = [x.strip() for x in lines]
+print(lines)
 
 the_sum = 0
 
-#for line in lines:
- #   print(line)
+for line in lines:
+    print(line)
 
 for line in lines:
     the_sum = the_sum + int(line)
     print(the_sum)
 
 print(sum([int(x) for x in lines]))
-  
+print("The sum is",the_sum)
+print("The sum is also",sum([int(x) for x in lines]))
+print(f"The sum is again {the_sum}")
+
